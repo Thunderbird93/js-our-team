877e291466d551976a2e2138cdffc6f4bfddda67
Thunderbird93 <antircristian@gmail.com>
2022-10-25 21:58:10 +0200

    [add] Milestone 2

--- js/app.js
+++ js/app.js
@@ -3,47 +3,63 @@ const team = [
     {
     nome: 'Wayne Barnett',
     ruolo: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: '../img/wayne-barnett-founder-ceo.jpg',
     },
 
     {
     nome: 'Angela Caroll',
     ruolo: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: '../img/angela-caroll-chief-editor.jpg',
     },
 
     {
     nome: 'Walter Gordon',
     ruolo: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: '../img/walter-gordon-office-manager.jpg',
     },
 
     {
     nome: 'Angela Lopez',
     ruolo: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: '../img/angela-lopez-social-media-manager.jpg',
     },
 
     {
     nome: 'Scott Estrada',
     ruolo: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: '../img/scott-estrada-developer.jpg',
     },
 
     {
     nome: 'Barbara Ramos',
     ruolo: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: '../img/barbara-ramos-graphic-designer.jpg',
     }, 
 ]
 
 
-//Milestone 1
+/*
+Milestone 1
 for (let employee of team){
     console.log(employee)
 }
+*/
 
 //Milestone 2
+const row = document.querySelector(".row");
+for (let employee of team){
+    let card = `
+    <div class="col-4">
+        <div class="card" style="width: 18rem;">
+            <img src="${employee.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${employee.nome}</h5>
+                <p class="card-text">${employee.ruolo}</p>
+            </div>
+        </div>
+    </div>`
 
+ row.innerHTML += card;
+}
 
 
